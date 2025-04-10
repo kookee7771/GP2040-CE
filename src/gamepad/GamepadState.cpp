785bb44b20aaf3f5878f4b16139b19b1576876dd
kookee7771 <67719916+kookee7771@users.noreply.github.com>
2025-04-10 17:33:12 +0900

    Update GamepadState.cpp

--- src/gamepad/GamepadState.cpp
+++ src/gamepad/GamepadState.cpp
@@ -1,3 +1,102 @@
+#include "GamepadState.h"
+#include "drivermanager.h"
+
+// Convert the horizontal GamepadState dpad axis value into an analog value
+uint16_t dpadToAnalogX(uint8_t dpad)
+{
+	switch (dpad & (GAMEPAD_MASK_LEFT | GAMEPAD_MASK_RIGHT))
+	{
+		case GAMEPAD_MASK_LEFT:
+			return GAMEPAD_JOYSTICK_MIN;
+
+		case GAMEPAD_MASK_RIGHT:
+			return GAMEPAD_JOYSTICK_MAX;
+
+		default:
+			if ( DriverManager::getInstance().getDriver() != nullptr )
+				return DriverManager::getInstance().getDriver()->GetJoystickMidValue();
+			else
+				return GAMEPAD_JOYSTICK_MID;
+	}
+}
+
+// Convert the vertical GamepadState dpad axis value into an analog value
+uint16_t dpadToAnalogY(uint8_t dpad)
+{
+	switch (dpad & (GAMEPAD_MASK_UP | GAMEPAD_MASK_DOWN))
+	{
+		case GAMEPAD_MASK_UP:
+			return GAMEPAD_JOYSTICK_MIN;
+
+		case GAMEPAD_MASK_DOWN:
+			return GAMEPAD_JOYSTICK_MAX;
+
+		default:
+			if ( DriverManager::getInstance().getDriver() != nullptr )
+				return DriverManager::getInstance().getDriver()->GetJoystickMidValue();
+			else
+				return GAMEPAD_JOYSTICK_MID;
+	}
+}
+
+uint8_t getMaskFromDirection(DpadDirection direction)
+{
+	return dpadMasks[direction-1];
+}
+
+uint8_t updateDpad(uint8_t dpad, DpadDirection direction)
+{
+	static bool inList[] = {false, false, false, false, false}; // correspond to DpadDirection: none, up, down, left, right
+	static list<DpadDirection> dpadList;
+
+	if(dpad & getMaskFromDirection(direction))
+	{
+		if(!inList[direction])
+		{
+			dpadList.push_back(direction);
+			inList[direction] = true;
+		}
+	}
+	else
+	{
+		if(inList[direction])
+		{
+			dpadList.remove(direction);
+			inList[direction] = false;
+		}
+	}
+
+	if(dpadList.empty()) {
+		return 0;
+	}
+	else {
+		return getMaskFromDirection(dpadList.back());
+	}
+}
+
+/**
+ * @brief Filter diagonals out of the dpad, making the device work as a 4-way lever.
+ *
+ * The most recent cardinal direction wins.
+ *
+ * @param dpad The GameState.dpad value.
+ * @return uint8_t The new dpad value.
+ */
+uint8_t filterToFourWayMode(uint8_t dpad)
+{
+	updateDpad(dpad, DIRECTION_UP);
+	updateDpad(dpad, DIRECTION_DOWN);
+	updateDpad(dpad, DIRECTION_LEFT);
+	return updateDpad(dpad, DIRECTION_RIGHT);
+}
+
+/**
+ * @brief Run SOCD cleaning against a D-pad value.
+ *
+ * @param mode The SOCD cleaning mode.
+ * @param dpad The GamepadState.dpad value.
+ * @return uint8_t The clean D-pad value.
+ */
 uint8_t runSOCDCleaner(SOCDMode mode, uint8_t dpad)
 {
     if (mode == SOCD_MODE_BYPASS) {
@@ -51,8 +150,8 @@ uint8_t runSOCDCleaner(SOCDMode mode, uint8_t dpad)
     
     // 중립 상태 강제 기간 확인
     if (forceNeutralState) {
-        // 중립 상태 기간이 10ms(약 1프레임 이상) 지속되었는지 확인
-        if (currentTime - neutralDuration >= 10) {
+        // 중립 상태 기간이 20ms(약  프레임) 지속되었는지 확인
+        if (currentTime - neutralDuration >= 13) {
             forceNeutralState = false;  // 중립 상태 종료
         } else {
             // 중립 상태 유지
